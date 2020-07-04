5b53a6351d21bbe7f1636c1f0b727909a3e12f68
Robert Pratersch <robert@pratersch.de>
2020-07-04 19:47:29 +0200

    testhml before li

--- README.md
+++ README.md
@@ -11,8 +11,7 @@
 
 ## Documantation
 
-It is uses the Freelancer template from  [StartBootstrap](https://github.com/StartBootstrap/startbootstrap-freelancer/) as starting point. The customizition is done with the resulting html and js files.
-I do not like to use jade and this .pug file to make changes.
+It is uses the Freelancer template from  [StartBootstrap](https://github.com/StartBootstrap/startbootstrap-freelancer/) as starting point. The customizition is done with the resulting html and js files. I do not like to use jade and this .pug file to make changes.
 It uses plain JS for fetching Todoist. In the current State the Bearer Token is hard coded in custom.js.
 So is also learning by doing and rapid development.
 
@@ -22,5 +21,6 @@ So is also learning by doing and rapid development.
 - [ ] Portfolio section
 - [ ] Activity section
 - [x] Test fetching todoist in test.html and custom.js
-- [ ] integrate blog
+- [x] Integrate own fontawsome Kit with own Account
+- [ ] Integrate blog
 - [ ] deploy to pratersch.de
--- js/custom.js
+++ js/custom.js
@@ -9,23 +9,49 @@ var requestOptions = {
   headers: myHeaders,
   redirect: 'follow'
 };
-
-
-
-fetch("https://api.todoist.com/rest/v1/tasks?project_id=2239338509", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-      for(let task of result){
-          let el = document.createElement("li");
-          if (task.section_id==13973542) {
-            el.innerText = task.content;
-            document.getElementById('shortterm').appendChild(el);
-            console.log(task.content);     
-          } 
-         
-      }  
-      //console.log(result)
-    })
+//fetch projectlist in a Promise
+var projects = fetch("https://api.todoist.com/rest/v1/projects",requestOptions)
+  .then(r => r.json())
+  
   .catch(error => console.log('error', error));
 
-  console.log("custom.js end")
+projects.then(res => {
+  for(let p of res){
+    console.log("P_ID: "+ p.id+" P_name: "+p.name);
+    fetchTasksFromProject(p.id);
+    if (p.id == 13973542) {}
+  }  
+})
+
+function fetchTasksFromProject(p_id){
+  fetch("https://api.todoist.com/rest/v1/tasks?project_id="+p_id, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    for (let task of result) {
+      
+        console.log(task.content);
+        let li = document.createElement("li");
+        let el = document.createElement("input");
+        let label = document.createElement("label");
+
+        li.setAttribute("class","list-group-item")
+        el.setAttribute("type", "checkbox");
+        el.setAttribute("readonly", "true");
+        label.setAttribute("for", task.content);
+        label.innerText = task.content;
+
+        div.appendChild(el);
+        div.appendChild(label);
+
+        document.getElementById('shortterm').appendChild(div);
+       
+     
+    }
+    //console.log(result)
+  })
+  .catch(error => console.log('error', error));
+}
+
+
+
+console.log("custom.js end")
